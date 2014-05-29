6001f50f841ed9b3238ae511d0887b2f5cadea6c
Henrik Joreteg <hjoreteg@gmail.com>
2014-05-29 16:52:45 -0700

    so let it begin

---
+++ key-tree-store.js
@@ -0,0 +1,39 @@
+function KeyTreeStore() {
+    this.storage = {};
+}
+
+// add an object to the store
+KeyTreeStore.prototype.add = function (keypath, obj) {
+    var arr = this.storage[keypath] || (this.storage[keypath] = []);
+    arr.push(obj);
+};
+
+// remove an object
+KeyTreeStore.prototype.remove = function (obj) {
+    var path, arr;
+    for (path in this.storage) {
+        arr = this.storage[path];
+        arr.some(function (item, index) {
+            if (item === obj) {
+                arr.splice(index, 1);
+                return true;
+            }
+        });
+    }
+};
+
+// grab all relevant objects
+KeyTreeStore.prototype.get = function (keypath) {
+    var keys = Object.keys(this.storage);
+    var res = [];
+
+    keys.forEach(function (key) {
+        if (key.indexOf(keypath) !== -1) {
+            res = res.concat(this.storage[key]);
+        }
+    }, this);
+
+    return res;
+};
+
+module.exports = KeyTreeStore;
